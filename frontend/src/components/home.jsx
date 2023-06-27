7e42dfb918bbe2b8362a7bbea0142af33d393340
aashishjai <24100036@lums.edu.pk>
2023-06-28 02:57:54 +0500

    Front end made with three buttons

---
+++ frontend/src/components/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <button>Game 1</button>
+      <button>Game 2</button>
+      <button>Game 3</button>
+    </div>
+  );
+};
+
+export default Home;
