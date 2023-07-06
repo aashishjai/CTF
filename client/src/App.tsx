023345255f95b00556e578dd5d9e77dabc6335f6
aashishjai <24100036@lums.edu.pk>
2023-07-07 03:20:32 +0500

    made sql injection game

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,8 @@ import HomePage from './components/Home/Home';
 import XSS from './components/Games/xss';
 import PasswordCracker from './components/Games/passwordCracker';
 import SQLInjectionGame from './components/Games/sql';
+// import SQLInjectionGame2 from './components/Games/gg';
+import SqlGame from './components/Games/sqlGame';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 import SignUpPage from './components/Validation/Signup';
@@ -22,7 +24,8 @@ function App() {
             <Route path="home/:id" element={<Home />} />
             <Route path="game1/:id" element={<XSS />} />
             <Route path="game2/:id" element={<PasswordCracker />} />
-            <Route path="game3/:id" element={<SQLInjectionGame />} />
+            <Route path="game3/" element={<SqlGame />} />
+            {/* <Route path="game4/" element={<SQLInjectionGame />} /> */}
           </Route>   
         </Routes>
       </Router>
---
+++ client/src/components/Games/sqlGame.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import '../Styles/SqlGame.css';
+
+const SqlGame = () => {
+  const [input, setInput] = useState('');
+
+  const handleInputChange = (e:any) => {
+    setInput(e.target.value);
+  };
+
+  const handleAlert = () => {
+    const targetText = "Login was successful!  Welcome maxmiller!";
+    if (input === targetText) {
+      alert("Congratulations! You entered the correct flag!");
+    } else {
+      alert("Incorrect flag. Please try again.");
+    }
+  };
+
+  return (
+    <div className="sql-game-container">
+      <h2 className="sql-game-title">SQL Injection Game</h2>
+      <p className="sql-game-instructions">Play three levels of the game in the link below and enter the flag here:</p>
+        <a className="sql-game-link" href="https://www.sql-insekten.de/">Click here for game</a>
+      <br />
+      <br />
+      <label className="sql-game-label">
+        <span className="sql-game-input-label">Flag:</span>
+        <input className="sql-game-input" type="text" value={input} onChange={handleInputChange} />
+      </label>
+      <br />
+      <button className="sql-game-submit" onClick={handleAlert}>Submit</button>
+    </div>
+  );
+};
+
+export default SqlGame;
